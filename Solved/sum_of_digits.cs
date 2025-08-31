2af074a153c3fd51cbb7058e027d37b5bcb979ea
Harigovind K <harigovindofficial@gmail.com>
2025-08-31 14:22:52 +0530

    Update sum_of_digits.cs

--- Solved/sum_of_digits.cs
+++ Solved/sum_of_digits.cs
@@ -4,20 +4,14 @@ class sum_of_digits
     static void Main()
     {
         Console.Write("Enter number: ");
-        int num = Convert.ToInt32(Console.ReadLine());
-        int len = num.ToString().Length;
-        int[] num_array = new int[len];
-        for (int i = 0; i < len; i++)
+        int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+        int sum = 0;
+        while (num != 0)
         {
-            num_array[i] = num % 10;
+            sum = sum + (num % 10);
             num = num / 10;
         }
-        int sum = 0;
-        for (int i = 0; i < len; i++)
-        {
-            sum += num_array[i];
-        }
-        System.Console.WriteLine($"Sum = {sum}");
 
+        Console.WriteLine($"Sum is {sum}");
     }
 }
