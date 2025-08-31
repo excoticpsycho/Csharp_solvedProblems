2319220346926c29dc7fe0ee907b6b297c20b9df
Harigovind K <harigovindofficial@gmail.com>
2025-08-31 15:00:31 +0530

    Create secondLargest_inArray.cs

---
+++ Solved/secondLargest_inArray.cs
@@ -0,0 +1,25 @@
+//⦁	Given an array, find the second largest number without sorting.
+using System;
+class second_largest_arr
+{
+    static void Main()
+    {
+        int[] arr = { 1, 23, 78, 56, 56, 44 };
+        int largest = int.MinValue;
+        int second_largest = int.MinValue;
+        foreach (int num in arr)
+        {
+            if (num > largest)
+            {
+                second_largest = largest;
+                largest = num;
+            }
+            else if (num > second_largest && num != largest)
+            {
+                second_largest = num;
+            }
+        }
+        Console.WriteLine($"largest {largest}");
+        Console.WriteLine($"second largest {second_largest}");
+    }
+}
