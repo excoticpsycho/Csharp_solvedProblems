7472350150d572c1f483fe20ece005afd84f0747
Harigovind K <harigovindofficial@gmail.com>
2025-08-31 14:25:03 +0530

    Update sum_of_digits.cs

--- Solved/sum_of_digits.cs
+++ Solved/sum_of_digits.cs
@@ -9,7 +9,7 @@ class sum_of_digits
         int sum = 0;
         while (num != 0)
         {
-            sum = sum + (num % 10);
+            sum += (num % 10);
             num = num / 10;
         }
 
