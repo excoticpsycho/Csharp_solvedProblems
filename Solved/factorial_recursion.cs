a94ba0e16f741b89380b904e67e2b2f3f2ad2c70
Harigovind K <harigovindofficial@gmail.com>
2025-08-31 13:34:05 +0530

    factorial-recursion_and_iterative

--- Solved/factorial_recursion.cs
+++ Solved/factorial_recursion.cs
@@ -6,14 +6,26 @@ class factorial
     static void Main()
     {
         int num = 10;
-         System.Console.WriteLine(factorial_num(num)); 
+        System.Console.WriteLine(factorial_num_recursion(num));
+        System.Console.WriteLine(factorial_num(num));
+
 
     }
-    static int factorial_num(int num)
+    static int factorial_num_recursion(int num)
     {
         if (num == 1)
             return 1;
         else
             return num * factorial_num(num - 1);
+    }
+    static int factorial_num(int num)
+    {
+        int result;
+        for (int i = 1; i <= num; i++)
+        {
+            result *= i;
+
+        }
+        return result;
     }
 }
