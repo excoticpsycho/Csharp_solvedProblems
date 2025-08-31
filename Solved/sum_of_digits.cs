ac59a69d1c6a7863dddd3f483f175524cfd3cf85
Harigovind K <harigovindofficial@gmail.com>
2025-08-31 14:14:13 +0530

    sum_of_numbers_of_digits

---
+++ Solved/sum_of_digits.cs
@@ -0,0 +1,23 @@
+//Given a number, find the sum of its digits.
+class sum_of_digits
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int len = num.ToString().Length;
+        int[] num_array = new int[len];
+        for (int i = 0; i < len; i++)
+        {
+            num_array[i] = num % 10;
+            num = num / 10;
+        }
+        int sum = 0;
+        for (int i = 0; i < num_array.Length; i++)
+        {
+            sum += num_array[i];
+        }
+        System.Console.WriteLine($"Sum = {sum}");
+
+    }
+}
